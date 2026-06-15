7787d6b9c79d6e3d873ebf1262ba0b5154d36c4a
imran <immui1413@gmail.com>
2026-06-15 18:09:03 +0530

    selectio sort

---
+++ 8pg.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+    int a[20],n,i,j,pos,temp,small;
+    printf("enter the length of the array : ");
+    scanf("%d",&n);
+    printf("enter the elements for the array : ");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n-1;i++){
+        small =  a[i];
+        pos = a[i];
+        pos=i;
+       
+
+        for(j=i+1;j<n;j++){
+            if(a[j]<small){
+                small=a[j];
+                pos=j;
+            }
+        }
+        temp = a[pos];
+        a[pos]=a[i];
+        a[i]=temp;
+
+    }
+    printf("sorted elelemts r : ");
+    for(i=0;i<n;i++){
+        printf("%d\t",a[i]);
+    }
+}
