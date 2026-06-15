659dd4855f13606d720cfcc8124a348a22c64842
imran <immui1413@gmail.com>
2026-06-16 00:23:15 +0530

    leanear search

---
+++ 11pg.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    int n, i, j, a[20], temp;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    
+    // Insertion sort logic
+    for(i = 1; i < n; i++){
+        temp = a[i];
+        j = i - 1;
+        
+        while(j >= 0 && a[j] > temp){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = temp;
+    }
+    
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+}
---
+++ 12pg.c
@@ -0,0 +1,24 @@
+// leanear search 
+#include<stdio.h>
+int main(){
+    int n,a[20],i,pos,search;
+    printf("enter the size of the arra");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+    printf("enter the element to find : ");
+    scanf("%d",&search);
+    for(i=0;i<n;i++){
+        if(a[i]==search){
+            pos = 1;
+            break;
+        }
+    }
+    if(pos){
+    printf("found at location %d",i+1);
+    }else{
+    printf("not found");
+    }
+}
